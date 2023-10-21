5c4a4d78abc8abd4e1fa3be4af8e4a6378d03544
Estudio <cruz.ezequiel.p@gmail.com>
2023-10-21 17:15:15 -0300

    Se dio estilos a la vista de detalles

--- src/components/detail/detail.component.ts
+++ src/components/detail/detail.component.ts
@@ -9,10 +9,52 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class DetailComponent {
   constructor(private rutaActiva: ActivatedRoute) { }
 
-  idParams:any=''
+  idParams:any = ''
 
   ngOnInit() {
     this.idParams = this.rutaActiva.snapshot.params["id"]
-    console.log( this.idParams)
+  }
+
+  products:any=[
+    {
+      id:1,
+      name:"Notebook Gamer Asus ROG Zephyrus G14 QHD 14 Ryzen 7 4800HS 16GB (2x8GB) 512GB SSD NVMe GTX 1650 W10 120Hz",
+      images:'./assets/images/notebook1.jpg',
+      characteristics:"La notebook es una potente herramienta tecnológica portátil que combina un diseño delgado y elegante con un rendimiento excepcional. Equipada con un procesador de última generación y una pantalla de alta resolución, esta notebook es ideal para usuarios que buscan un equilibrio perfecto entre estilo y productividad",
+      description:'Esta notebook está equipada con un amplio almacenamiento interno que te permite guardar una gran cantidad de archivos, desde documentos importantes hasta tu colección de música y películas. Con su generosa capacidad de almacenamiento, no tendrás que preocuparte por quedarte sin espacio en el disco duro.',
+      link:'/productos/notebook1',
+      price:720000
+      },
+    {
+      id:2,
+      name:"Notebook 2",
+      images:'./assets/images/notebook2.jpg',
+      characteristics:"Esta notebook es una máquina versátil que se adapta a múltiples necesidades, desde la toma de apuntes en el aula hasta la edición de fotos y videos en casa. Su diseño compacto y ligero la convierte en la compañera perfecta para aquellos que están en constante movimiento, mientras que su batería de larga duración garantiza una productividad ininterrumpida.",
+      description:'La notebook es una solución de almacenamiento ultrarrápida gracias a su unidad de estado sólido (SSD) de alta velocidad. Con esta tecnología, tus aplicaciones se cargarán en cuestión de segundos, y podrás acceder a tus archivos de manera eficiente,lo que la convierte en una opción ideal para usuarios que valoran la velocidad y el rendimiento.',
+      price:720000
+    }
+    ,
+    {
+      id:3,
+      name:"Notebook 3",
+      images:'./assets/images/notebook3.jpg',
+      characteristics:"La notebook es una herramienta esencial para profesionales y creativos que exigen un rendimiento excepcional. Con un potente procesador y una tarjeta gráfica de alto rendimiento, notebook permite ejecutar aplicaciones intensivas y realizar tareas exigentes con facilidad. Su pantalla de alta definición ofrece una experiencia visual envolvente.",
+      description:"Si eres un amante de la fotografía o el diseño gráfico, esta notebook te ofrece un generoso espacio de almacenamiento en su unidad de disco duro, ideal para guardar tus archivos de alta resolución. Ya no tendrás que preocuparte por la capacidad de almacenamiento cuando captures imágenes o trabajes en proyectos creativos.",
+      price:720000
+    }
+    ,
+    {
+      id:4,
+      name:"Notebook 4",
+      images:'./assets/images/notebook4.jpg',
+      characteristics:"Si buscas una notebook que ofrezca una experiencia de entretenimiento inigualable, esta es la elección perfecta. Con una pantalla vibrante y un sistema de audio de alta calidad, esta notebook te sumerge en películas, juegos y contenido multimedia. Además, su conectividad avanzada te permite disfrutar de una amplia gama de entretenimiento en línea.",
+      description:"La notebook se destaca por su versatilidad de almacenamiento, ya que combina tanto un disco duro tradicional de gran capacidad como una unidad SSD rápida para un rendimiento óptimo. Esto significa que puedes almacenar tus archivos pesados en el disco duro y mantener tus aplicaciones y archivos de acceso frecuente en el SSD, brindándote un equilibrio perfecto entre capacidad y velocidad.",
+      price:720000
+    }
+  ]
+
+  
+  get filterProducts(): any {
+    return this.products.filter(((item: { id: any; })=>item.id==this.idParams))
   }
 }
