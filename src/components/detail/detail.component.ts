6ecb3a3f7b819682b3a156ae66b779ccae240a5b
Estudio <cruz.ezequiel.p@gmail.com>
2023-10-21 17:44:03 -0300

    Se formateo el precio

--- src/components/detail/detail.component.ts
+++ src/components/detail/detail.component.ts
@@ -23,38 +23,53 @@ export class DetailComponent {
       characteristics:"La notebook es una potente herramienta tecnológica portátil que combina un diseño delgado y elegante con un rendimiento excepcional. Equipada con un procesador de última generación y una pantalla de alta resolución, esta notebook es ideal para usuarios que buscan un equilibrio perfecto entre estilo y productividad",
       description:'Esta notebook está equipada con un amplio almacenamiento interno que te permite guardar una gran cantidad de archivos, desde documentos importantes hasta tu colección de música y películas. Con su generosa capacidad de almacenamiento, no tendrás que preocuparte por quedarte sin espacio en el disco duro.',
       link:'/productos/notebook1',
-      price:720000
+      price:720400,
+      code:'SKU: GA401IHR-K2053T'
       },
     {
       id:2,
-      name:"Notebook 2",
+      name:"Notebook Lenovo ThinkPad E14 FHD 14 Ryzen 7 5700U 8GB 256GB SSD NVMe Freedos",
       images:'./assets/images/notebook2.jpg',
       characteristics:"Esta notebook es una máquina versátil que se adapta a múltiples necesidades, desde la toma de apuntes en el aula hasta la edición de fotos y videos en casa. Su diseño compacto y ligero la convierte en la compañera perfecta para aquellos que están en constante movimiento, mientras que su batería de larga duración garantiza una productividad ininterrumpida.",
       description:'La notebook es una solución de almacenamiento ultrarrápida gracias a su unidad de estado sólido (SSD) de alta velocidad. Con esta tecnología, tus aplicaciones se cargarán en cuestión de segundos, y podrás acceder a tus archivos de manera eficiente,lo que la convierte en una opción ideal para usuarios que valoran la velocidad y el rendimiento.',
-      price:720000
+      price:52135,
+      code:'SKU: GA401IHR-K2053T'
     }
     ,
     {
       id:3,
-      name:"Notebook 3",
+      name:"Notebook ASUS X515EA 15.6 FHD Core I3 1115G4 4GB 256GB SSD NVMe Freedos",
       images:'./assets/images/notebook3.jpg',
       characteristics:"La notebook es una herramienta esencial para profesionales y creativos que exigen un rendimiento excepcional. Con un potente procesador y una tarjeta gráfica de alto rendimiento, notebook permite ejecutar aplicaciones intensivas y realizar tareas exigentes con facilidad. Su pantalla de alta definición ofrece una experiencia visual envolvente.",
       description:"Si eres un amante de la fotografía o el diseño gráfico, esta notebook te ofrece un generoso espacio de almacenamiento en su unidad de disco duro, ideal para guardar tus archivos de alta resolución. Ya no tendrás que preocuparte por la capacidad de almacenamiento cuando captures imágenes o trabajes en proyectos creativos.",
-      price:720000
+      price:85695,
+      code:'SKU: GA401IHR-K2053T'
     }
     ,
     {
       id:4,
-      name:"Notebook 4",
+      name:"Notebook Asus Vivobook M3500 OLED FHD 15.6 Ryzen 5 5600H 8GB 512GB SSD NVMe W11 Home",
       images:'./assets/images/notebook4.jpg',
       characteristics:"Si buscas una notebook que ofrezca una experiencia de entretenimiento inigualable, esta es la elección perfecta. Con una pantalla vibrante y un sistema de audio de alta calidad, esta notebook te sumerge en películas, juegos y contenido multimedia. Además, su conectividad avanzada te permite disfrutar de una amplia gama de entretenimiento en línea.",
       description:"La notebook se destaca por su versatilidad de almacenamiento, ya que combina tanto un disco duro tradicional de gran capacidad como una unidad SSD rápida para un rendimiento óptimo. Esto significa que puedes almacenar tus archivos pesados en el disco duro y mantener tus aplicaciones y archivos de acceso frecuente en el SSD, brindándote un equilibrio perfecto entre capacidad y velocidad.",
-      price:720000
+      price:68452,
+      code:'SKU: GA401IHR-K2053T'
     }
   ]
 
   
   get filterProducts(): any {
     return this.products.filter(((item: { id: any; })=>item.id==this.idParams))
+  }
+
+  formatedPrice(price :any) {
+    return price.toLocaleString('es-ES', { style: 'currency', currency: 'ARS',minimumFractionDigits: 0,
+    maximumFractionDigits: 0, });
+  }
+
+  roundPrice(price :any) {
+    let quotas= Math.round(price /12)
+    return quotas.toLocaleString('es-ES', { style: 'currency', currency: 'ARS',  minimumFractionDigits: 0,
+    maximumFractionDigits: 0, });
   }
 }
